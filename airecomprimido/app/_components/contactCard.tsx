97b721f6242966998ffcc32d8b8481f3ffefcfec
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-02-05 17:52:15 +0300

    Add header style

--- airecomprimido/app/_components/contactCard.tsx
+++ airecomprimido/app/_components/contactCard.tsx
@@ -1,20 +1,30 @@
-export default function ContactCard()
+export default function ContactCard({
+  state,
+}: {
+  state: string
+})
 {
+  const titleStyle = "text-(--dark-blue) text-xl font-medium"
+  const infoStyle = "font-light text-lg ml-4"
+  const buttonStyle = "min-w-35 text-center py-2 px-4 rounded-4xl text-white font-medium hover:cursor-pointer transition-all duration-300"
+
   return (
-    <div className="contact-card">
-      <span>X</span> {/*Close icon*/}
-      <h3>Información de contacto</h3>
-      <h4>Dirección</h4>
-      <p></p>
-      <button>Abrir en el mapa</button>
-      <h4>Números de contacto</h4>
-      <p>(02) 2021262 / 0991848293</p>
-      <h4>Correo electrónico</h4>
-      <p>ventas@airecomprimidoec.com</p>
-      <div className="social-section">
-        <p>Escríbenos</p>
-        <span>whatsapp</span>
-        <span>Correo</span>
+    <div className={`absolute ${state} flex-col top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg p-10`}> {/*Contact Card*/ }
+      <span className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-(--dark-blue) w-7 h-7 text-white text-center rounded-4xl hover:cursor-pointer">X</span> {/*Close icon*/}
+      <h3 className="text-(--dark-blue) text-center text-3xl font-medium mb-4">Información de contacto</h3>
+      <h4 className={titleStyle}>Dirección</h4>
+      <p className={infoStyle}>José Mogro Oe10D s/n y Matilde Hidalgo</p>
+      <button className={`${buttonStyle} w-45 self-center my-3 bg-(--light-blue) hover:bg-(--dark-blue)`}>Abrir en el mapa</button>
+      <h4 className={titleStyle}>Números de contacto</h4>
+      <p className={infoStyle}>(02) 2021262 / 0991848293</p>
+      <h4 className={titleStyle}>Correo electrónico</h4>
+      <p className={infoStyle}>ventas@airecomprimidoec.com</p>
+      <div className="w-full border-t-3 mt-7 pt-4">
+        <p className="text-center text-(--dark-blue) text-xl">Escríbenos</p>
+        <div className="flex justify-center px-5 my-3 gap-15">
+          <span className={`bg-(--green) hover:bg-(--hover-green) ${buttonStyle}`}>WhatsApp</span>
+          <span className={`bg-(--red) hover:bg-(--hover-red) ${buttonStyle}`}>Correo</span>
+        </div>
       </div>
     </div>
   )
--- airecomprimido/app/_components/header.tsx
+++ airecomprimido/app/_components/header.tsx
@@ -1,26 +1,40 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import ContactCard from "./contactCard";
+import { useState } from "react";
 
 export default function Header() 
 {
+  const linkStyle = "text-white text-lg font-medium  transition-all duration-100 hover:scale-110"
+
+  const [cardState, setcardState] = useState("hidden")
+
   return (
-    <header>
-      <Image 
-        src="/logos/logo.png"
-        alt="AireComprimido EC S.A.S"
-        width={250}
-        height={250}
-        priority
-      />
-      <div className="nav-container">
-        <ul>
-          <li><Link href="/">Inicio</Link></li>
-          <li><Link href="/equipos">Equipos</Link></li>
-          <li><Link href="/repuestos">Repuestos</Link></li>
-          <li><Link href="/mantenimiento">Mantenimiento</Link></li>
-        </ul>
-        <button>Contáctanos</button>
-      </div>
-    </header>
+    <>
+      <header className="fixed w-full bg-(--light-blue) flex h-16 px-10 py-2.5">
+        <Link href="/">
+          <Image 
+            src="/logos/logo.png"
+            alt="AireComprimido EC S.A.S"
+            width={1383}
+            height={1598}
+            className="h-full w-auto"
+            priority
+          />
+        </Link>
+        <div className="mr-5 ml-auto flex">
+          <nav className="flex items-center gap-10 mr-15">
+            <Link href="/" className={linkStyle}>Inicio</Link>
+            <Link href="/equipos" className={linkStyle}>Equipos</Link>
+            <Link href="/repuestos" className={linkStyle}>Repuestos</Link>
+            <Link href="/mantenimiento" className={linkStyle}>Mantenimiento</Link>
+          </nav>
+          <button className="bg-white text-(--dark-blue) rounded-4xl px-5 text-lg font-medium hover:bg-(--dark-blue) hover:text-white transition-all duration-300 hover:cursor-pointer">Contáctanos</button>
+        </div>
+      </header>
+      <ContactCard state={cardState}/>
+    </>
   );
 }
