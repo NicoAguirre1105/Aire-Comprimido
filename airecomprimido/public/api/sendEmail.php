5f653c12b5ebbfcac28e53cf3de24251ac4a8d0c
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-02-13 02:07:28 +0300

    Small fixes to deploy to prod

--- airecomprimido/public/api/sendEmail.php
+++ airecomprimido/public/api/sendEmail.php
@@ -1,22 +1,33 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://www.airecomprimidoec.com");
+$allowedOrigins = [
+  "https://airecomprimidoec.com",
+  "https://www.airecomprimidoec.com",
+  "http://localhost:3000"
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+  header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
+
+include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   http_response_code(200);
   exit();
 }
 
-$to = 'ventas@airecomprimidoec.com';
+$to = 'ventas-uio@airecomprimidoec.com';
 
 $data = json_decode(file_get_contents('php://input'));
 
 if (!empty($data->email) && !empty($data->subject) && !empty($data->message)) {
 
-  $message = "Este es un email es fue enviado desde tu sitio web.\n\n Un visitante ha dejado el siguiente mensaje: Correo electrónico del autor: {$data->email}.\n Mensaje: {$data->message} \n\n Fecha: " . date('Y-m-d H:i:s');
+  $message = "Ha recibido un correo desde el sitio web de airecomprimidoec.com.\n\nEl correo es:\n {$data->email}.\n\nEl mensage es:\n {$data->message} \n\n Fecha: " . date('Y-m-d H:i:s');
   $headers = "From: no-reply@airecomprimidoec.com\r\n"; 
 
   if(mail($to, $data->subject, $message, $headers)) {
