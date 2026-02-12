c1aeebb8ed45d27c30cd8236731b81296cd18b04
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-02-12 20:01:07 +0300

    Change fetch route

--- airecomprimido/app/_components/emailForm.tsx
+++ airecomprimido/app/_components/emailForm.tsx
@@ -20,7 +20,7 @@ export default function EmailForm({
     setError('');
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sendEmail.php`, {
+      const res = await fetch(`/api/sendEmail.php`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, subject, message }),
