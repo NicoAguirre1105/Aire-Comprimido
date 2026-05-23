1b636f3eaacd4fed244ae1526855058b372207e5
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-05-22 19:00:45 -0500

    Changes inside aside
    - Future features deleted
    - Fix log out button position

--- airecomprimido/app/(admin)/_components/aside.tsx
+++ airecomprimido/app/(admin)/_components/aside.tsx
@@ -41,57 +41,59 @@ export default function Aside({
   }
 
   return (
-    <aside className={`${isOpen || !isMobile ? 'left-0' : '-left-full'} ${asideStyle} bg-(--dark-blue) px-10 py-8 flex flex-col transition-all duration-200 ease-in-out sm:px-10 md:w-2/5 md:max-w-80 md:min-w-60`}>
-      <Image
-        src="/icons/right_arrow_white.svg"
-        alt="Right arrow icon"
-        width={150}
-        height={150}
-        className={`absolute md:hidden top-7 right-7 w-10 h-auto hover:scale-110 transition-all duration-200 cursor-pointer`}
-        onClick={toggleAside}
-      />
-      <Image
-        src="/logos/logo.png"
-        alt="AIRECOMPRIMIDO EC logo"
-        width={150}
-        height={150}
-        className="h-15 w-fit mb-20"
-      />
-      <nav className="text-white flex flex-col text-xl font-semibold gap-7 py-5">
-        <Link href='/reportes' className={hyperStyle}>
-          <Image
-            src="/icons/docs.svg"
-            alt="Docs icon"
-            width={150}
-            height={150}
-            className={hyperIconStyle}
-          />
-          Reportes
-        </Link>
-        <Link href='/generador_qr' className={hyperStyle}>
-          <Image
-            src="/icons/qr_code.svg"
-            alt="QR code icon"
-            width={150}
-            height={150}
-            className={hyperIconStyle}
-          />
-          Generador QR
-        </Link>
-      </nav>
-      <button 
-        onClick={handleLogout}
-        className="text-(--red) font-medium text-left mt-auto mb-5 flex items-center gap-1"
-      >
+    <aside className={`${isOpen || !isMobile ? 'left-0' : '-left-full'} ${asideStyle} bg-(--dark-blue) flex flex-col transition-all duration-200 ease-in-out sm:px-10 md:w-2/5 md:max-w-80 md:min-w-60`}>
+      <div className="md:fixed flex flex-col h-dvh px-10 py-8 md:px-0 md:py-10">
         <Image
-            src="/icons/exit_red.svg"
-            alt="Exit"
-            width={150}
-            height={150}
-            className={hyperIconStyle}
-          />
-        Cerrar Sesión
-      </button>
+          src="/icons/right_arrow_white.svg"
+          alt="Right arrow icon"
+          width={150}
+          height={150}
+          className={`absolute md:hidden top-7 right-7 w-10 h-auto hover:scale-110 transition-all duration-200 cursor-pointer`}
+          onClick={toggleAside}
+        />
+        <Image
+          src="/logos/logo.png"
+          alt="AIRECOMPRIMIDO EC logo"
+          width={150}
+          height={150}
+          className="h-15 w-fit mb-20"
+        />
+        <nav className="text-white flex flex-col text-xl font-semibold gap-7 py-5">
+          <Link href='/reportes' className={hyperStyle}>
+            <Image
+              src="/icons/docs.svg"
+              alt="Docs icon"
+              width={150}
+              height={150}
+              className={hyperIconStyle}
+            />
+            Reportes
+          </Link>
+          {/* <Link href='/generador_qr' className={hyperStyle}>
+            <Image
+              src="/icons/qr_code.svg"
+              alt="QR code icon"
+              width={150}
+              height={150}
+              className={hyperIconStyle}
+            />
+            Generador QR
+          </Link> */}
+        </nav>
+        <button 
+          onClick={handleLogout}
+          className="text-(--red) font-medium text-left mt-auto mb-5 flex items-center gap-1"
+        >
+          <Image
+              src="/icons/exit_red.svg"
+              alt="Exit"
+              width={150}
+              height={150}
+              className={hyperIconStyle}
+            />
+          Cerrar Sesión
+        </button>
+      </div>
     </aside>
   )
 }
