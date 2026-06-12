3d77d1aa91cd266665c9856d6837ae3b3e737dcf
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-06-12 18:09:49 -0500

    Update version

--- airecomprimido/app/(public)/_components/footer.tsx
+++ airecomprimido/app/(public)/_components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer()
     <footer className="bg-black flex flex-col py-10 px-10 text-center items-center max-sm:py-7">
       <p className={textStyle}>AIRECOMPRIMIDO EC S.A.S. Todos los derechos reservados © 2021</p>
       <p className={textStyle}>Sitio web elaborado por Nicolas Aguirre</p>
-      <p className={`${textStyle} text-xs`}>v2.0.0</p>
+      <p className={`${textStyle} text-xs`}>v2.0.1</p>
     </footer>
   );
 }
