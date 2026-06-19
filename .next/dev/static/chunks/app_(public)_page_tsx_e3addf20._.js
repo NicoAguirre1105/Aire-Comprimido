f78fead877eb4b0ba1214826a4183742e79f2334
Nicolas Aguirre <nicofrancis2002@gmail.com>
2026-06-19 09:54:41 -0500

    Save versions deployed

--- .next/dev/static/chunks/app_(public)_page_tsx_e3addf20._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_(public)__components_6a0ac645._.js"
-],
-    source: "dynamic"
-});
